565d85cc16af4e86de75a8d9205edd5c5af4016d
Rkevjoker <88000934+Rkevjoker@users.noreply.github.com>
2021-09-06 22:43:46 -0600

    ...

---
+++ Ejercicio1/Ejercicio1/Entidad/ClsCompra.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1.Entidad
+{
+    class ClsCompra
+    {
+        private string dia;
+        private double compra;
+        private double descuento;
+        private double total1;
+        private double total2;
+
+        public ClsCompra(string dia, double compra, double descuento, double total1, double total2)
+        {
+            this.Dia = dia;
+            this.Compra = compra;
+            this.Descuento = descuento;
+            this.Total1 = total1;
+            this.Total2 = total2;
+        }
+
+        public string Dia { get => dia; set => dia = value; }
+        public double Compra { get => compra; set => compra = value; }
+        public double Descuento { get => descuento; set => descuento = value; }
+        public double Total1 { get => total1; set => total1 = value; }
+        public double Total2 { get => total2; set => total2 = value; }
+    }
+}
---
+++ Ejercicio1/Ejercicio1/Negocio/NClsCompra.cs
@@ -0,0 +1,29 @@
+﻿using Ejercicio1.Entidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1.Negocio
+{
+    class NClsCompra
+    {   
+        public String descuento(ClsCompra Cls)
+        {
+            Cls.Descuento = (0.15 * Cls.Compra);
+            Cls.Total1 = (Cls.Descuento - Cls.Compra);
+            Cls.Total2 = (Cls.Compra);
+            {
+                if (Cls.Dia == martes || Cls.Dia == viernes)
+
+            }
+
+
+  
+        }
+            
+
+
+    }
+}
